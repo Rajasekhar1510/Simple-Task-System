04fd7df132a8aa5f6fbce2bda97a2589cf5fefd0
pvs20csu283 <rajasekkhar2609@gmail.com>
2025-06-28 21:35:52 +0530

    NPC Dialogue Basics

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+
+public class NPC : MonoBehaviour, IInteractable
+{
+    public NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+
+    public int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+
+    }
+
+    public void Interact()
+    {
+       if (dialogueData != null || !isDialogueActive)
+       {
+            return;
+       }
+
+
+       if (isDialogueActive)
+       {
+            NextLine();
+       }
+       else
+       {
+            StartDialogue();
+       }
+
+    }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.npcName);
+
+        dialoguePanel.SetActive(true);
+
+        //TypeLines
+
+        StartCoroutine(TypeLine());
+
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if (++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            //end dialogue
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText(" ");
+
+        foreach (char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if (dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "NewNPCDialogue", menuName = "NPC DIALOGUE")]
+public class NPCDialogue : ScriptableObject
+{
+    public string npcName;
+    public GameObject npcModel;
+    public string[] dialogueLines;
+    public bool[] autoProgressLines;
+    public float autoProgressDelay = 1.5f;
+    public float typingSpeed = 0.05f;
+    //public AudioClip voiceSound;
+   // public float voicePitch = 1f;
+
+
+
+}
